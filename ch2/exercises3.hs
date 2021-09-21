73a79697e3419368c6e8b2782b1471128513fc58
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-09-21 18:16:22 -0300

    new exercises

---
+++ ch2/exercises3.hs
@@ -0,0 +1,24 @@
+{-
+   The following code samples are broken and won’t compile. The first
+   two are as you might enter into the REPL; the third is from a source
+   file. Find the mistakes and fix them so that they will.
+-}
+
+{-
+1. let area x = 3. 14 * (x * x)
+2. let double x = b * 2
+-}
+
+-- solutions:
+-- let area x = 3.14 * (x * x)
+-- let double x = x * 2
+
+{-
+   x = 7
+    y = 10
+   f = x + y
+-}
+
+x = 7
+y = 10
+f = x + y
---
+++ ch2/learn.hs
@@ -0,0 +1,11 @@
+-- learn.hs
+-- declaring values in a file
+module Learn where
+	-- First, we declare the name of our module
+	-- os it can be imported by name in a project
+	
+x = 10 * 5 + y
+
+myResult = x * 5
+
+y = 10
